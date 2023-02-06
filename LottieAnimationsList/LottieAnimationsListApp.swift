8992ae0c88ad02ebbf88218da90ae06ad07b45a2
Isaad Khan <IKhan@lcs.on.ca>
2023-02-05 20:11:21 -0500

    Initial Commit

---
+++ LottieAnimationsList/LottieAnimationsListApp.swift
@@ -0,0 +1,17 @@
+//
+//  LottieAnimationsListApp.swift
+//  LottieAnimationsList
+//
+//  Created by Isaad Khan on 2023-02-05.
+//
+
+import SwiftUI
+
+@main
+struct LottieAnimationsListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
